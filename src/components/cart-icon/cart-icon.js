2384044da4ee7b40f500d6e45f61f135ff513b7f
Jesus Rincon <jrincon_externo@rayensalud.com>
2019-08-28 16:52:39 -0400

    added reselect for optimization

--- src/components/cart-icon/cart-icon.js
+++ src/components/cart-icon/cart-icon.js
@@ -4,13 +4,16 @@ import { connect } from 'react-redux';
 import { toggleCartDropdown } from '../../redux/cart/cartActions';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+
+import { selectCartItemsCount } from '../../redux/cart/cartSelectors';
+
 import './cart-icon.scss';
 
-const CartIcon = ({ toggleCartDropdown }) => {
+const CartIcon = ({ toggleCartDropdown, itemCount }) => {
   return (
     <div className='cart-icon' onClick={toggleCartDropdown}>
       <ShoppingIcon className='shopping-icon' />
-      <span className='item-count'>0</span>
+      <span className='item-count'> {itemCount} </span>
     </div>
   );
 };
@@ -19,7 +22,11 @@ const mapDispatchToProps = dispatch => ({
   toggleCartDropdown: () => dispatch(toggleCartDropdown())
 });
 
+const mapStateToProps = state => ({
+  itemCount: selectCartItemsCount(state)
+});
+
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(CartIcon);
