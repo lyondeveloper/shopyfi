b825795d5f3779422736bbfaf4b87dc4a37b2f51
Jesus Rincon <contactojesuserincon@gmail.com>
2019-09-29 20:50:34 -0300

    conectando commits

--- src/components/collection-preview/collection-preview.js
+++ src/components/collection-preview/collection-preview.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
 
 import CollectionItem from '../collection-item/collection-item';
 
 import './collection-preview.scss';
 
-const CollectionPreview = ({ title, items, history }) => {
+const CollectionPreview = ({ title, items }) => {
   return (
     <div className='collection-preview'>
-      <h1
-        className='title'
-        onClick={() => history.push(`/shop/${title.toLowerCase()}`)}
-      >
-        {title.toUpperCase()}
-      </h1>
+      <h1 className='title'>{title.toUpperCase()}</h1>
       <div className='preview'>
         {items
           .filter((item, index) => index < 4)
@@ -25,4 +19,4 @@ const CollectionPreview = ({ title, items, history }) => {
   );
 };
 
-export default withRouter(CollectionPreview);
+export default CollectionPreview;
