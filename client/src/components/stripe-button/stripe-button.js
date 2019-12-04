0aa31a75570f1b84d387515a959b9adf4ca1bcec
Jesus Rincon <jrincon_externo@rayensalud.com>
2019-12-04 17:06:52 -0300

    added doms things

--- client/src/components/stripe-button/stripe-button.js
+++ client/src/components/stripe-button/stripe-button.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import { toast } from 'react-toastify';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishedKey = 'pk_test_HIdraAeWb9qjmOjvt4hzU7X7';
 
   const onToken = token => {
-    console.log(token);
-    toast.success('Payment Successful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then(res => {
+        toast.success('Payment Successful');
+      })
+      .catch(err => {
+        console.log('Payment Error: ', JSON.parse(err));
+        toast.error(
+          'There was an unexpected error, please be sure you use the provided credit card properly'
+        );
+      });
   };
 
   return (
